a79ae352a75136f799a77d7b153999dca2b0f2db
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-27 20:19:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
       @total -= (@total - (@total * (employee_discount / 100)))
       "After the discount, the total comes to #{@total}."
     else
-      
+      "There is no discount to apply."
   end
   
   
