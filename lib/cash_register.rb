fdff6190c35711776170babce1031d29b30f0f85
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-27 20:25:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
   
   def initialize(discount = 0)
     @total = 0
-    @employee_discount = discount
+    @discount = discount
   end
   
   def add_item(title, price, quantity = 1)
